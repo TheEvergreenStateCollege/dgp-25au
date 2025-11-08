c1b463c1ad99d76cf6fa7132ecbc3c22005738c2
talmudicdeer <lavkyl06@evergreen.edu>
2025-11-07 21:39:17 -0800

    Spacing changes

--- homeworks/AlexisHW/Pool.java
+++ homeworks/AlexisHW/Pool.java
@@ -8,20 +8,23 @@ class PoolPuzzle {
             if (x < 1) {
                 System.out.print(" ");
             }
+
             System.out.print("n");
 
             if (x > 1) {
                 System.out.print(" oyster");
                 x = x + 2;
             }
+
             if (x == 1) {
                 System.out.print("noys");
             }
+
             if (x < 1) {
                 System.out.print("oise");
             }
-            System.out.println();
 
+            System.out.println();
             x = x + 1;
         }
     }
