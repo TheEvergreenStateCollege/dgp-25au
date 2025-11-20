6ea31a9fec41a8956330c256a79ccb02f04e8f06
WrenW01 <235224754+WrenW01@users.noreply.github.com>
2025-11-19 19:55:26 -0800

    added pseudocode for SimpleStartupGame for real this time

---
+++ activities/week-08/SimpleStartupGame.java
@@ -0,0 +1,10 @@
+public class SimpleStartupGame {
+  //Intialize an object of type SimpleStartup
+  //Intialize an interger array of length 3 and feed it to the SimpleStartup SetLocationCells method
+  //Declare a counter interger
+  //Wrap everything in a while loop depending on results not equalling kill
+    //Print a message asking the user for a guess and import the user input into an interger
+    //Intalize the results variable and have its value be the result of feeding the interger guess into the checkYourself function  
+    //Increment counter interger with each run through the loop
+  //Print out the counter interger along with an explanation of the purpose
+}
