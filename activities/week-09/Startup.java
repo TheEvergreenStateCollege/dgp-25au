0ab58255ca71a9bc0666398391f7e233a6f28a89
WrenW01 <235224754+WrenW01@users.noreply.github.com>
2025-12-03 19:26:50 -0800

    added comments to code

--- activities/week-09/Startup.java
+++ activities/week-09/Startup.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class Startup {
+    //Startups instance variable an array list of cell locations and the startup's name
     private ArrayList<String> locationCells;
     private String name;
 
@@ -8,27 +9,34 @@ public class Startup {
         this.name = name;
     }
 
+    //A setter method that updates the startup's location (random location provided by GameHelper placeStartup() method)
     public void setLocationCells(ArrayList<String> loc) {
         locationCells = loc;
     }
 
+    //Your basic setter method
     public void setName(String n) {
         name = n;
     }
 
     public String checkYourself(String userInput) {
         String result = "miss";
+        //The array list index of method in action if the user guess is one of the entries in the array list indexOf will return the array list location, if not indexOf will return -1
         int index = locationCells.indexOf(userInput);
         if (index >= 0) {
+            //Using array list's remove method to delete an entry
             locationCells.remove(index);
 
+            //Using the isEmpty method to see if all the locations have been guessed
             if (locationCells.isEmpty()) {
                 result = "kill";
+                //Tell the user when a startup has been sunk
                 System.out.println("Ouch! You sunk " + name + " :( ");
             } else {
                 result = "hit";
             }
         }
+        //return hit or miss or kill
         return result;
     }
 }
