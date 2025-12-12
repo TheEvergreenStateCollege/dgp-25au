c218e36a0cdd8714fe08a60b8f3e28e038f285f1
talmudicdeer <lavkyl06@evergreen.edu>
2025-12-12 15:57:45 -0800

    Edited SimpleStartup.java in line with HW 9

--- activities/week-08/SimpleStartup.java
+++ activities/week-08/SimpleStartup.java
@@ -24,4 +24,39 @@ public class SimpleStartup {
         locationCells = loc;
         numOfHits = 0;
     }
+
+    public void thisIsHowIWin() {
+        int firstHitIndex = -1;
+
+       for (int i = 0; i <= 6; i++) {
+            System.out.println("Guessing: " + i);
+            String result = checkYourself(i);
+            System.out.println("Result: " + result);
+            if (result.equals("hit") || result.equals("kill!")) {
+                firstHitIndex = i;
+                break;
+            }
+        }
+
+        if (firstHitIndex == -1) {
+            System.out.println("No hits found.");
+            return;
+        }
+
+        for (int j = firstHitIndex + 1; j <= 6; j++) {
+            System.out.println("Guessing: " + j);
+            String result = checkYourself(j);
+            System.out.println("Result: " + result);
+            if (result.equals("miss")) break;
+            if (result.equals("kill!")) return;
+        }
+
+        for (int j = firstHitIndex - 1; j >= 0; j--) {
+            System.out.println("Guessing: " + j);
+            String result = checkYourself(j);
+            System.out.println("Result: " + result);
+            if (result.equals("miss")) break;
+            if (result.equals("kill!")) return;
+        }
+    }
 }
