e0d0e546200143ce76b009b83a83b41c5aa111f4
talmudicdeer <lavkyl06@evergreen.edu>
2025-12-12 14:29:13 -0800

    Changed SimpleStartupGame.java for Homework 8.

--- activities/week-08/SimpleStartupGame.java
+++ activities/week-08/SimpleStartupGame.java
@@ -1,38 +1,32 @@
 import java.util.Scanner;
 
 public class SimpleStartupGame {
-  //Intialize an object of type SimpleStartup
-  //Intialize an interger array of length 3 and feed it to the SimpleStartup SetLocationCells method
-  //Declare a counter interger
-  //Wrap everything in a while loop depending on results not equalling kill
-    //Print a message asking the user for a guess and import the user input into an interger
-    //Intalize the results variable and have its value be the result of feeding the interger guess into the checkYourself function  
-    //Increment counter interger with each run through the loop
-  //Print out the counter interger along with an explanation of the purpose
+
     public static void main (String[] args) {
         SimpleStartup dot =new SimpleStartup();
+
         int random = (int) (Math.random() * 5);
         int[] locations = { random, random + 1, random + 2 };
         dot.setLocationCells(locations);
+
         int counter = 0;
         Scanner scanner = new
         Scanner(System.in);
         String result = "";
         int guess = -1;
 
-        do {
+        while (!result.equals("kill!")) {
             System.out.print("Submit a guess: ");
-            if (result == "hit" && guess > 0) {
-                --guess;
-            } else {
-                guess = (int) (Math.random() * 7);
-            }
-            //int guess = scanner.nextInt();
+
+            guess = Scanner.nextInt();
+
             result = dot.checkYourself(guess);
-            System.out.println("guess: " + guess + ", result: " + result);
-        } while (!result.equals("kill!"));
 
+            counter++;
 
+            System.out.println("Your guess was: " + guess ", and the result was " + result + "!");
+        }
 
+        System.out.println("You sunk the startup in " + counter + " guesses!");
     }
 }
